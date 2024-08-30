de9fd19a53d30f20e51d226babdddbcfd2223dc6
Scott Brunswig <sbrunswig@gmail.com>
2024-08-30 10:38:41 -0500

    update pacakge

--- stores/website.ts
+++ stores/website.ts
@@ -21,10 +21,9 @@ export const useWebsiteStore = defineStore("website", {
       // const res = await fetch("/cges/test.json");
       // const json = await res.blob();
       // console.log("Getting site data", json);
-      const wb = await this.parse_from_url("/cges/content.xlsx");
+      const wb = await this.parse_from_url("https://cges-zeta.vercel.app/cges/content.xlsx");
       // const sheet = wb.Sheets[wb.SheetNames[0]];
-      for (const s of wb.SheetNames)
-        this.data[s] = XLSX.utils.sheet_to_json(wb.Sheets[s]);
+      for (const s of wb.SheetNames) this.data[s] = XLSX.utils.sheet_to_json(wb.Sheets[s]);
       console.log("sitedata", this.data);
 
       //if (json) Object.assign(this.data, json);
