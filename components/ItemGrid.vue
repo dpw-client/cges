7046b687f6bc9575b25e0d80def6a20353500564
Scott Brunswig <sbrunswig@gmail.com>
2024-10-17 15:00:34 -0500

    michael updates

--- components/ItemGrid.vue
+++ components/ItemGrid.vue
@@ -10,7 +10,7 @@
           :value="item"
           :active="item == filter"
           rounded="xl"
-          color="secondary"
+          :color="site?.AppColors?.[0]?.Primary ?? '#b1b7c1'"
           @click="() => filterBy(item)"
         >
           <template v-slot:prepend>
@@ -27,39 +27,27 @@
         <b>{{ content.length }}</b> Found:
       </div>
       <div class="d-flex flex-wrap ga-6 justify-left pa-4">
-        <!-- title	subtitle	abstract_image	abstract	body	attachments	is_dialog	type -->
         <v-card
           v-for="c of content"
           :key="c"
-          :style="`width: 300px; border-top: 2px solid ${cardTypes[c.type]};`"
-          @click="openURL(c.attachments)"
+          :style="`width: 300px; border-top: 2px solid ${cardTypes[c.Type]};`"
+          @click="openURL(c.Attachments)"
         >
           <v-card-item>
-            <v-card-title v-if="'title' in c" style="text-wrap: wrap">
-              <b>{{ unquote(c.title) }}</b>
+            <v-card-title v-if="'Title' in c" style="text-wrap: wrap">
+              <b>{{ unquote(c.Title) }}</b>
             </v-card-title>
-            <v-card-subtitle v-if="'subtitle' in c">{{
-              unquote(c.subtitle)
+            <v-card-subtitle v-if="'Subtitle' in c">{{
+              unquote(c.Subtitle)
             }}</v-card-subtitle>
-            <template v-slot:prepend v-if="'abstract_image' in c">
+            <template v-slot:prepend v-if="'Image' in c">
               <v-avatar size="64" rounded="0">
-                <v-img :src="`/cges/img/${c.abstract_image}`"></v-img>
+                <v-img :src="`/cges/img/${c.Image}`"></v-img>
               </v-avatar>
             </template>
           </v-card-item>
           <v-card-text>
-            <div v-if="'abstract' in c">{{ c.abstract }}</div>
-            <!-- <div v-if="'attachments' in c"> -->
-            <!-- <div v-for="(a, i) in parseURLs(c.attachments)"> -->
-            <!-- <div
-                class="pt-3"
-                @click="openURL(a)"
-                style="font-weight: 700; cursor: pointer"
-              >
-                Link {{ i + 1 }}
-              </div> -->
-            <!-- </div> -->
-            <!-- </div> -->
+            <div v-if="'Abstract' in c">{{ c.Abstract }}</div>
           </v-card-text>
         </v-card>
       </div>
@@ -67,38 +55,21 @@
   </div>
 </template>
 
-<script setup>
-// import { useWebsiteStore } from "~~/stores/website";
-// const website = useWebsiteStore();
-// const siteData = website.data;
-</script>
+<script setup></script>
 
 <script>
 import { mapStores } from "pinia";
 import { useWebsiteStore } from "~~/stores/website";
-// import siteData from "~/public/cges/localization.json";
 
 export default {
   props: {
-    // items: {
-    //   type: Array,
-    //   required: true,
-    //   default: Array,
-    // },
     contentKey: {
       type: String,
       required: true,
       default: "",
     },
   },
-  async mounted() {
-    //globalThis.CGEScontent.cardTypes
-    // console.log("mw", await $fetch("/cges/localization.json"));
-    // for (const t of siteData["Card Types"]) this.cardTypes[t.type] = t.color;
-    // this.all = siteData[this.contentKey];
-    // this.content = this.all;
-    // this.types = [...new Set(this.content.map((d) => d.type.trim()))];
-  },
+  async mounted() {},
   data() {
     return {
       cardTypes: {},
@@ -115,7 +86,7 @@ export default {
       return this.fcontent || this.all;
     },
     types() {
-      return [...new Set(this.all.map((d) => d.type.trim()))];
+      return [...new Set(this.all.map((d) => d.Type.trim()))];
     },
   },
   methods: {
@@ -127,9 +98,6 @@ export default {
         return str.slice(1, -1); // Return the substring
       return str;
     },
-    // parseURLs(s) {
-    //   return s.split(" ").filter((u) => u != "");
-    // },
     openURL(url) {
       if (url.startsWith("http")) window.open(url, "_blank");
       else window.open("/cges/pdf/" + url, "_blank");
@@ -138,7 +106,7 @@ export default {
       console.log("here");
       this.filter = type;
       if (type == "All") this.fcontent = null;
-      else this.fcontent = this.all.filter((o) => o.type == type);
+      else this.fcontent = this.all.filter((o) => o.Type == type);
     },
   },
 };
--- pages/wodr.vue
+++ pages/wodr.vue
@@ -15,9 +15,33 @@
     </v-card-subtitle>
 
     <v-card-text>
+      <v-select
+        id="countrySelect"
+        v-model="selectedCountry"
+        label="Select a Country"
+        density="compact"
+        :items="countryList"
+        item-title="Name"
+        item-value="ISO"
+        @update:modelValue="onCountrySelect"
+        class="w-25 mt-10 ml-8"
+        style="min-width: 200px"
+      />
+      <div class="w-25 ml-8">
+        <div
+          style="
+            min-width: 200px;
+            margin-top: -10px;
+            position: absolute;
+            background-color: #fff9;
+            width: fit-content;
+          "
+          v-html="hint"
+        />
+      </div>
       <svg-map
         :map="World"
-        class="ma-10"
+        class="ma-10 mt-0"
         :location-class="getLocationClass"
         @mouseover="pointLocation"
         @mouseout="unpointLocation"
@@ -26,16 +50,12 @@
       <div
         class="examples__block__map__tooltip"
         :style="
-          'position: fixed; width: 200px; padding: 10px; border: 1px solid darkgray; background-color: white; ' +
+          'position: fixed; width: fit-content; padding: 10px; border: 1px solid darkgray; background-color: rgba(255, 255, 255, 0.95); ' +
           tooltipStyle
         "
       >
-        {{ pointedLocation }}
+        <div v-html="pointedLocation" />
       </div>
-      MAP nav w mouse/kbd
-      <br />
-      legend: Abolitionist, Abolitionist for common law crimes, De facto
-      abolitionist, Retentionist
     </v-card-text>
 
     <v-card-subtitle class="d-flex justify-center">
@@ -46,17 +66,15 @@
         World Coalition Against the Death Penalty.
       </div>
     </v-card-subtitle>
-
-    <!-- <v-card-actions>
-      <v-btn color="primary">Learn More</v-btn>
-      <v-btn color="secondary">Share</v-btn>
-    </v-card-actions> -->
   </v-card>
 </template>
 
 <script>
 // import il from "../components/ItemList.vue";
 // import ig from "../components/ItemGrid.vue";
+import { mapStores } from "pinia";
+import { useWebsiteStore } from "~~/stores/website";
+
 import { SvgMap } from "vue3-svg-map";
 import World from "./world.js";
 
@@ -67,13 +85,82 @@ export default {
     SvgMap,
   },
   props: {},
-  mounted() {},
   data() {
-    return { World, pointedLocation: null, tooltipStyle: null };
+    return {
+      World,
+      pointedLocation: null,
+      tooltipStyle: null,
+      mapData: null,
+      selectedCountry: null,
+      hint: "",
+    };
+  },
+  watch: {
+    "websiteStore.data.AppColors"() {
+      this.setColors();
+    },
+  },
+  computed: {
+    ...mapStores(useWebsiteStore),
+    countryList() {
+      return this.websiteStore?.data["WODR Map"] || [];
+    },
+    locationKey() {
+      const lk = {};
+      const mapData = this.websiteStore.data["WODR Map"];
+      (mapData || []).forEach((l) => (lk[l.ISO] = l));
+      return lk;
+    },
+  },
+  // beforeRouteEnter(to, from, next) {
+  //   console.log("enter");
+  //   this.setColors();
+  //   next();
+  // },
+  updated() {
+    console.log("updated");
+    this.setColors();
   },
   methods: {
+    addDynamicRule(el, color) {
+      // Dynamically create a style tag
+      const style = document.createElement("style");
+      style.type = "text/css";
+      //style["data-dyncss"] = true;
+      style.dataset.dyncss = true;
+
+      // Define your dynamic CSS rule
+      const cssRule = `
+        ${el} {
+          fill: ${color};
+        }
+      `;
+
+      // Insert the CSS rule into the style tag
+      style.appendChild(document.createTextNode(cssRule));
+
+      // Append the style tag to the document head
+      document.head.appendChild(style);
+    },
+    setColors() {
+      const existing = document.querySelectorAll(`style[data-dyncss]`);
+      if (existing.length) return;
+
+      this.addDynamicRule();
+      console.log("Set Colors");
+      const colors = this.websiteStore?.data["AppColors"];
+      if (!colors || colors?.length == 0) return;
+
+      for (let i = 0; i < 5; i++) {
+        if (colors[0][`map-color-${i}`])
+          this.addDynamicRule(`.map-color-${i}`, colors[0][`map-color-${i}`]);
+      }
+    },
     getLocationName(node) {
       if (node?.attributes?.name) return node && node.attributes.name.value;
+    },
+    getLocationID(node) {
+      if (node?.attributes?.id) return node && node.attributes.id.value;
     },
     getSelectedLocationName(map, locationId) {
       return (
@@ -82,7 +169,20 @@ export default {
       );
     },
     pointLocation(event) {
+      if (!event.target.attributes?.name) return;
       this.pointedLocation = this.getLocationName(event.target);
+      if (this.pointedLocation) {
+        const id = this.getLocationID(event.target).toUpperCase();
+        const lk = this.locationKey[id];
+        if (lk) {
+          this.pointedLocation += `<br/>Women: ${lk.WODR}<br/>Crime: ${
+            lk.Crime || "N/A"
+          }<br/>Type: ${lk.Legend}`;
+        } else {
+          // CGES has no data
+          this.pointedLocation += `<br/>Women: N/A<br/>Crime: N/A`;
+        }
+      }
     },
     unpointLocation(event) {
       this.pointedLocation = null;
@@ -94,14 +194,68 @@ export default {
       //   top: `${event.clientY + 10}px`,
       //   left: `${event.clientX - 100}px`,
       // };
-      this.tooltipStyle = `display:block; top:${event.clientY + 10}px; left:${
-        event.clientX - 100
-      }px;`;
+      // console.log(event);
+      if (this.pointedLocation) {
+        this.selectedCountry = null;
+        this.hint = "";
+        this.tooltipStyle = `display:block; top:${event.clientY + 10}px; left:${
+          event.clientX - 100
+        }px;`;
+      } else this.tooltipStyle = "";
     },
     getLocationClass(location, index) {
       // Generate heat map
-      return `svg-map__location svg-map__location--heat${index % 4}`;
+      const legend = [
+        "",
+        "Abolitionist",
+        "Abolitionist for common law crimes",
+        "De facto abolitionist",
+        "Retentionist",
+      ];
+      const id = location.id.toUpperCase();
+      const lk = this.locationKey[id];
+      if (lk) {
+        return `svg-map__location map-color-${legend.indexOf(lk.Legend)}`;
+      } else {
+        return `svg-map__location map-color-0`;
+      }
+    },
+    onCountrySelect(ISO) {
+      const lk = this.locationKey[ISO];
+      if (lk) {
+        this.hint = `<b>Women:</b> ${lk.WODR} <br/><b>Crime:</b> ${
+          lk.Crime || "N/A"
+        } <br/><b>Type:</b> ${lk.Legend}`;
+      } else {
+        // CGES has no data
+        this.hint += `<br/>Women: N/A<br/>Crime: N/A`;
+      }
+      return;
     },
   },
 };
 </script>
+
+<style>
+.svg-map__location {
+  stroke: rgba(0, 0, 0, 0.2);
+  stroke-width: 1px;
+  transition: fill 1s;
+}
+
+.map-color-0 {
+  fill: #b1b7c1;
+}
+/* .map-color-1 {
+  fill: #75d7cd;
+}
+.map-color-2 {
+  fill: #0071c5;
+}
+.map-color-3 {
+  fill: #004792;
+}
+.map-color-4 {
+  fill: #2c3968;
+} */
+</style>
